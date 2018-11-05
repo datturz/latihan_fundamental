dd17b1eef6c3d73f6515c1783e4d424c08096897
crosoveer <hidayattur07@gmail.com>
2018-11-05 10:50:50 +0700

    pertama

---
+++ app.js
@@ -0,0 +1,18 @@
+
+//V 1.0 Done Launce
+
+var pangkat = (x,y) =>{
+    if(y ==1){
+        return x
+    }else{
+        return x= x*pangkat(x, y-1)
+    }
+}
+
+
+console.log(2,3)
+
+/* 
+ 
+dua nih
+*/
